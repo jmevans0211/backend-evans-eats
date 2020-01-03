b38442f1cd0f6602fc6ae40d6e2c0bf13bc924ba
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2020-01-03 15:15:53 -0700

    Add data file for seeding

---
+++ data.js
@@ -0,0 +1,95 @@
+const data = {
+  categories: [
+    {
+      category_name: 'Apps'
+    },
+    {
+      category_name: 'Soups and Salads'
+    },
+    {
+      category_name: 'Meals'
+    },
+    {
+      category_name: 'Desserts'
+    },
+    {
+      category_name: 'Beverages'
+    },
+  ],
+  recipes: [
+    {
+      recipe_name: 'Tart',
+      approx_time: 'Tart Time',
+      ingredients: 'ingredient 1, ingrdient 2, ingredient 3',
+      instructions: 'Sweet roll macaroon cotton candy tart danish soufflé powder. Cotton candy biscuit cake icing marshmallow cookie. Sugar plum halvah halvah chocolate brownie caramels oat cake danish cupcake. Pie chocolate cake apple pie ice cream.',
+      notes: 'Tart notes go here',
+      image_url: 'https://res.cloudinary.com/dhieqoasj/image/upload/v1578078513/evans_eats/ffejlov2rpyvwmiftusc.jpg',
+      category: 'Desserts'
+    },
+    {
+      recipe_name: 'Macrons',
+      approx_time: 'Marcron Minutes',
+      ingredients: 'ingredient 1, ingrdient 2, ingredient 3',
+      instructions: 'Sweet roll macaroon cotton candy tart danish soufflé powder. Cotton candy biscuit cake icing marshmallow cookie. Sugar plum halvah halvah chocolate brownie caramels oat cake danish cupcake. Pie chocolate cake apple pie ice cream.',
+      notes: 'Macaron notes go here',
+      image_url: 'https://res.cloudinary.com/dhieqoasj/image/upload/v1578078446/evans_eats/jzfex3hctffm8qsqqtlt.jpg',
+      category: 'Desserts'
+    },
+    {
+      recipe_name: 'Curry',
+      approx_time: 'Curry Counter',
+      ingredients: 'ingredient 1, ingrdient 2, ingredient 3',
+      instructions: 'Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic. Gumbo beet greens corn soko endive gumbo gourd.',
+      notes: 'Curry notes go here',
+      image_url: 'https://res.cloudinary.com/dhieqoasj/image/upload/v1578083044/evans_eats/igeexwpxz8qff9mawbx4.jpg',
+      category: 'Meals'
+    },
+    {
+      recipe_name: 'Egg Toast',
+      approx_time: 'Toast Time',
+      ingredients: 'ingredient 1, ingrdient 2, ingredient 3',
+      instructions: 'Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic. Gumbo beet greens corn soko endive gumbo gourd.',
+      notes: 'Toast notes go here',
+      image_url: 'https://res.cloudinary.com/dhieqoasj/image/upload/v1578078297/evans_eats/tzb8hmrb6hdkklkuicpg.jpg',
+      category: 'Meals'
+    },
+    {
+      recipe_name: 'Ramen',
+      approx_time: 'Ramen Runtime',
+      ingredients: 'ingredient 1, ingrdient 2, ingredient 3',
+      instructions: 'Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic. Gumbo beet greens corn soko endive gumbo gourd.',
+      notes: 'Ramen notes go here',
+      image_url: 'https://res.cloudinary.com/dhieqoasj/image/upload/v1578078355/evans_eats/t3eqvgxq0wtq7wz1n4ha.jpg',
+      category: 'Soups and Salads'
+    },
+    {
+      recipe_name: 'Salmon Cracker App',
+      approx_time: 'Cracker Clock',
+      ingredients: 'ingredient 1, ingrdient 2, ingredient 3',
+      instructions: 'Asiatic glassfish longfin, California flyingfish redside tiger shovelnose catfish suckermouth armored catfish. Oarfish scorpionfish flat loach treefish handfish mrigal Rainbowfish naked-back knifefish powen, moray eel! Goldfish porgy clownfish prickleback.',
+      notes: 'Salmon cracker notes go here',
+      image_url: 'https://res.cloudinary.com/dhieqoasj/image/upload/v1578078326/evans_eats/t6u9v0g3y21ncmhyexp7.jpg',
+      category: 'Apps'
+    },
+    {
+      recipe_name: 'Grilled Salmon',
+      approx_time: 'Salmon Seconds',
+      ingredients: 'ingredient 1, ingrdient 2, ingredient 3',
+      instructions: 'Asiatic glassfish longfin, California flyingfish redside tiger shovelnose catfish suckermouth armored catfish. Oarfish scorpionfish flat loach treefish handfish mrigal Rainbowfish naked-back knifefish powen, moray eel! Goldfish porgy clownfish prickleback.',
+      notes: 'Salmon notes go here',
+      image_url: 'https://res.cloudinary.com/dhieqoasj/image/upload/v1578078185/evans_eats/hgkwiujnaacb8lhy3bn8.jpg',
+      category: 'Meals'
+    },
+    {
+      recipe_name: 'Merlot',
+      approx_time: 'Merlot Months',
+      ingredients: 'ingredient 1, ingrdient 2, ingredient 3',
+      instructions: 'A complex cabernet is a good one. Thunderbird is a screw-top classic. Sangiovese – go ahead, pronounce it. More? Lorem vino, dollar store blend. The concept of foie gras begs a brash blanc.',
+      notes: 'Merlot notes go here',
+      image_url: 'https://res.cloudinary.com/dhieqoasj/image/upload/v1578078580/evans_eats/jrnluibwxgmstrtfur6c.jpg',
+      category: 'Beverages'
+    }
+  ]
+}
+
+module.exports = data;
