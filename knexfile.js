339e0685b664409bc819af5f7571e2d6c204dee8
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2020-01-28 15:13:20 -0700

    Add production database url

--- knexfile.js
+++ knexfile.js
@@ -5,10 +5,20 @@ module.exports = {
     useNullAsDefault: true,
     migrations: {
       directory: './db/migrations'
-    }
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
   },
-  seeds: {
-    directory: './db/seeds/dev'
+  production: {
+    client: 'pg',
+    connection: 'postgres://hhmyqmwyscuswo:d3bc44d057033e667d65ba379c70b035970cea471cd61c39bf3181b5af8ab01b@ec2-3-214-53-225.compute-1.amazonaws.com:5432/d17584cqcofem0',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
   },
-  useNullAsDefault: true
 };
